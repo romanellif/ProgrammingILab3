70178e2567666e74b89148215575a9d294e3034a
romanellif <romanellifxox@gmail.com>
2020-03-25 13:09:14 -0400

    Last Edit

--- Question4.java
+++ Question4.java
@@ -17,7 +17,11 @@ public class Question4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        /*This program will prompt the user to pick one of the following
+        calculation methods. The program will then calculate what the user has
+        chosen after he enters his inputs.
+        */
         Scanner input = new Scanner (System.in);
         
         System.out.println("Pick one of the following conversions:");
@@ -26,6 +30,49 @@ public class Question4 {
         System.out.println("3. Celsius to Farenheit");
         System.out.println("4. Milliliters to Liters");
         System.out.println("5. CAD to EUR");
+    
+        double option = input.nextDouble();
+        
+        /*The first one is a conversion method from meters to feet*/
+        if (option == 1) {
+            System.out.println("Input the meters:");
+            double meters = input.nextDouble();
+            double feet = meters * 3.28084;
+            System.out.println(meters + " meters converted into feet is "
+                               + " equal to " + Math.round(feet));
+        }
+        /*The second one is a conversion method from Canadian Dollars to American*/
+        if (option == 2) {
+            System.out.println("Input CAD amount");
+            double cad = input.nextDouble();
+            double usd = cad * 0.72;
+            System.out.println(cad + " Canadian dollars converted into American "
+                                + usd + " dollars.");
+        }
+        /*The third one will convert Celsius to Farenheit*/
+        if (option == 3) {
+            System.out.println("Input the temperature in Celsius:");
+            double celsius = input.nextDouble();
+            double farenheit = (celsius * (9/5)) + 32;
+            System.out.println(celsius + " degrees converted into Farenheit is "
+                               + " equal to " + farenheit);
+        }   
+        /*The fourth one will convert Milliliters to Liters*/
+        if (option == 4) {
+            System.out.println("Input the amount of Milliliters:");
+            double milliliters = input.nextDouble();
+            double liters = milliliters * 0.001;
+            System.out.println(milliliters + " millitiers converted to liters is "
+                               + " equal to " + Math.round(liters) + " liters.");
+        }
+        /*The fifth one will convert Canadian Dollars to Euros*/
+        if (option == 5) {
+            System.out.println("Enter the amount in CAD:");
+            double cad = input.nextDouble();
+            double usd = cad * 0.72;
+            System.out.println(cad + " Canadian dollars converted into American "
+                                + usd + " dollars.");
+        }
     }
     
 }
